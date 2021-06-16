3800be23d2c75775e532b2e0ecbe34e9b31feb64
Christian Johansen <cjohansen11@me.com>
2021-06-15 20:23:34 -0400

    Added additional server requests and styled-components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import GET from '../../../lib/index.js';
-
+import styled from 'styled-components';
 import Coins from './Coins.jsx';
-import './styles/app.css';
+
+const AllCoins = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 1400px;
+  margin: auto;
+`
 
 const App = () => {
   const [activeMarketData, setActiveMarketData] = useState([]);
   const [activeCoin, setActiveCoin] = useState([]);
+  const [searchBar, setSearchBar] = useState('');
+  const [coinList, setCoinList] = useState([]);
   const [count, setCount] = useState(0);
+
+  const handleSearch = (query) => {
+    for (let key of coinList) {
+      console.log(coinList[key])
+    }
+  }
 
   const getMarketData = async (currency) => {
     const marketData = await GET.markets('usd', 25);
@@ -18,10 +34,16 @@ const App = () => {
   const getCoinData = async (coinId) => {
     const coinData = await GET.coinData('cardano');
     setActiveCoin([coinData]);
-  }
+  };
+
+  const getCoinList = async () => {
+    const capturedCoinList = await GET.coinList();
+    setCoinList([...capturedCoinList]);
+  };
 
   useEffect( async () => {
     await getMarketData();
+    await getCoinList();
   }, []);
 
   useEffect( async () => {
@@ -34,12 +56,19 @@ const App = () => {
 
   return (
     <>
+    <nav>
+      <h2>This is the nav bar</h2>
+      <input type="text" onChange={e => {
+          setSearchBar(e.target.value);
+          handleSearch(e.target.value);
+        }}></input>
+    </nav>
     <div className={'crypto-tracker'}>Hello World I'm a CryptoTracking Web App</div>
-    <div className={'all-coins'}>
+    <AllCoins>
       {activeMarketData.map(eachCoin => (
         <Coins key={eachCoin.id} eachCoin={eachCoin} />
       ))}
-    </div>
+    </AllCoins>
     </>
   );
 };
--- client/src/components/Coins.jsx
+++ client/src/components/Coins.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Coin = styled.div`
+  height: 250px;
+  width: 250px;
+  background-color: skyblue;
+  margin: 10px;
+  border-radius: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+`
+
+const Title = styled.h4`
+  width: 100%;
+  margin: 0;
+`
+
+const AllH5 = styled.h5`
+  width: 100%;
+  margin: 0px 10px;
+`
+
+const Price = styled.h3`
+  width: 100%;
+  font-size: 40px;
+  text-align: center;
+`
+
+const Span = styled.span`
+  display: inherit;
+`
 
 const Coins = ({ eachCoin }) => {
   return (
     <>
-    <div className={'coin'}>
-      <h4>{eachCoin.name}</h4>
-      <h5>{eachCoin.symbol.toUpperCase()}</h5>
-      <h3>${eachCoin.current_price}</h3>
-      <span>
-        <h5>{eachCoin.price_change_percentage_24h}%</h5>
-        <h5>${eachCoin.price_change_24h}</h5>
-      </span>
-    </div>
+    <Coin>
+      <Title>{eachCoin.name}</Title>
+      <AllH5>{eachCoin.symbol.toUpperCase()}</AllH5>
+      <Price>${eachCoin.current_price}</Price>
+      <Span>
+        <AllH5>{eachCoin.price_change_percentage_24h}%</AllH5>
+        <AllH5>${eachCoin.price_change_24h}</AllH5>
+      </Span>
+    </Coin>
     </>
   );
 };
